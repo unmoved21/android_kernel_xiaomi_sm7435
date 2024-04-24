2e6f715036b2406ceb50e02d3310829e359087b1
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-04-24 13:28:33 +0200

    input: touchscreen: xiaomi: Add double tap gesture sysfs notify node
    Change-Id: I03caf63ba624d5211665ef956df6ea8b38b95a27

--- drivers/input/touchscreen/xiaomi/xiaomi_touch.h
+++ drivers/input/touchscreen/xiaomi/xiaomi_touch.h
@@ -135,6 +135,7 @@ struct xiaomi_touch {
 	struct mutex palm_mutex;
 	struct mutex prox_mutex;
 	struct mutex gesture_single_tap_mutex;
+	struct mutex gesture_double_tap_mutex;
 	struct mutex fod_press_status_mutex;
 	wait_queue_head_t wait_queue;
 };
@@ -203,6 +204,8 @@ extern void last_touch_events_collect(int slot, int state);
 int xiaomi_touch_set_suspend_state(int state);
 
 extern int notify_gesture_single_tap(void);
+
+extern int notify_gesture_double_tap(void);
 
 extern int update_fod_press_status(int value);
 

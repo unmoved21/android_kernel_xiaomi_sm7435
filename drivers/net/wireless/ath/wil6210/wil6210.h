e07bdb4b0b6af6f7e015c8571e0c7982035ef504
AdarshGrewal <adarshgrewal@gmail.com>
2025-06-12 00:15:20 +0530

    net: wil6210: Gaurd wil_led_blink_set usage for debugfs
    Change-Id: Ic58549ca4d03a53c1514c969639278d2b97c20a8

--- drivers/net/wireless/ath/wil6210/wil6210.h
+++ drivers/net/wireless/ath/wil6210/wil6210.h
@@ -1481,7 +1481,11 @@ int wil_tt_set(struct wil6210_priv *wil, const char *buf,
 	       size_t count);
 int wil_qos_weights_set(struct wil6210_priv *wil, const char *buf,
 			size_t count);
+#if defined(CONFIG_WIL6210_DEBUGFS)
 int wil_led_blink_set(struct wil6210_priv *wil, const char *buf);
+#else
+static inline int wil_led_blink_set(struct wil6210_priv *wil, const char *buf) { return 0; }
+#endif
 
 int wil_cid_fill_sinfo(struct wil6210_vif *vif, int cid,
 		       struct station_info *sinfo);
